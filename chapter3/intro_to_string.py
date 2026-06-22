6cfafe84a385980ae7b63d81b5254d7933b69cb2
Masum75-max <almasumabdullah414@gmail.com>
2026-06-22 20:09:07 +0600

    string done

---
+++ chapter3/intro_to_string.py
@@ -0,0 +1,30 @@
+
+name="masum"
+
+print(name[0])
+
+nameShort = name[0:3] # 0 to 3-1 will be included
+
+print(nameShort)
+
+print(nameShort)
+
+print(len(name))
+
+print(name.endswith("um"))
+print(name.startswith("mo"))
+print(name.capitalize()) # only first character ke boro kore jodi choto hater theke thake
+print(name.upper())
+print(name.lower())
+print(name.find('a')) # returns the index of the first occurance of a fromm the string
+print(name.count('m'))    # returns how many times m has occuredd
+
+about = "he is a good boy"
+
+about = about.replace("good","bad")
+
+print(about)
+
+para ="masum is a student of ruet cse \n but he is tensed about his future a lot.\n because everybody is chanting \"ai gonna take your job\""
+
+print(para)
